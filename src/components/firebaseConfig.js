5a574fda268e7fc188c024605dbac475083b99ed
Aleksei Bakai <a.bakai@daviann.com>
2018-01-23 14:54:40 +0200

    initial commit

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyAqt3Ed_YOQu2x-s05o2qKFxAvTtAYWEmk",
+    authDomain: "vue-employer-manager.firebaseapp.com",
+    databaseURL: "https://vue-employer-manager.firebaseio.com",
+    projectId: "vue-employer-manager",
+    storageBucket: "vue-employer-manager.appspot.com",
+    messagingSenderId: "692856350756"
+}
